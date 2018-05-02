6e54f051c1449302debe5d1049addfa72a74dc01
Dave Draper <ddraper@atlassian.com>
2018-05-02 20:59:15 +1000

    Field refactor to add field wrapper

--- src/components/fields/atlaskit/FieldText.js
+++ src/components/fields/atlaskit/FieldText.js
@@ -1,82 +1,43 @@
 // @flow
 import React from 'react';
 import FieldText from '@atlaskit/field-text';
-import InfoIcon from '@atlaskit/icon/glyph/info';
-import Tooltip from '@atlaskit/tooltip';
-import styled from 'styled-components';
-import { FormContext } from '../../Form';
+import FieldWrapper from './FieldWrapper';
 import type { Field, FieldDef } from '../../../types';
 
-const Layout = styled.div`
-  > div {
-    display: inline-block;
-  }
-
-  > div:nth-child(2) {
-    margin-left: 10px;
-    line-height: 32px;
-    vertical-align: bottom;
-  }
-`;
-
 class AtlaskitFieldText extends React.Component<Field> {
-  constructor(props: Field) {
-    super(props);
-    const { registerField, onFieldChange, ...fieldDef } = props;
-    if (registerField) {
-      registerField(fieldDef);
-    } else {
-      console.warn(
-        'Could not register field because registerField function was missing',
-        fieldDef
-      );
-    }
-  }
   render() {
-    const { id, fields = [], onFieldChange } = this.props;
-    const fieldToRender = fields.find(field => field.id === id);
-    if (fieldToRender && fieldToRender.visible) {
-      const {
-        disabled,
-        errorMessages,
-        id,
-        isValid,
-        name,
-        placeholder,
-        required,
-        value,
-        label,
-        description
-      } = fieldToRender;
-      return (
-        <Layout id={id}>
-          <FieldText
-            key={id}
-            name={name}
-            label={label}
-            placeholder={placeholder}
-            disabled={disabled}
-            required={required}
-            isInvalid={!isValid}
-            invalidMessage={errorMessages}
-            value={value}
-            onChange={(evt: any) => onFieldChange(id, evt.target.value)}
-          />
-          {description && (
-            <Tooltip content={description} position="right">
-              <InfoIcon primaryColor="#6554C0" />
-            </Tooltip>
-          )}
-        </Layout>
-      );
-    }
-
-    return null;
+    const {
+      disabled,
+      errorMessages,
+      id,
+      isValid,
+      name,
+      onFieldChange,
+      placeholder,
+      required,
+      value,
+      label
+    } = this.props;
+    return (
+      <FieldText
+        key={id}
+        name={name}
+        label={label}
+        placeholder={placeholder}
+        disabled={disabled}
+        required={required}
+        isInvalid={!isValid}
+        invalidMessage={errorMessages}
+        value={value}
+        onChange={(evt: any) => onFieldChange(id, evt.target.value)}
+      />
+    );
   }
 }
 
 export default (props: FieldDef) => (
-  <FormContext.Consumer>
-    {form => <AtlaskitFieldText {...form} {...props} />}
-  </FormContext.Consumer>
+  <FieldWrapper {...props}>
+    {/* $FlowFixMe */}
+    <AtlaskitFieldText />
+  </FieldWrapper>
 );
--- src/components/fields/atlaskit/Select.js
+++ src/components/fields/atlaskit/Select.js
@@ -1,37 +1,10 @@
 // @flow
 import React from 'react';
 import SingleSelect from '@atlaskit/single-select';
-import InfoIcon from '@atlaskit/icon/glyph/info';
-import Tooltip from '@atlaskit/tooltip';
-import styled from 'styled-components';
-import { FormContext } from '../../Form';
+import FieldWrapper from './FieldWrapper';
 import type { Field, FieldDef } from '../../../types';
 
-const Layout = styled.div`
-  > div {
-    display: inline-block;
-  }
-
-  > div:nth-child(2) {
-    margin-left: 10px;
-    line-height: 32px;
-    vertical-align: bottom;
-  }
-`;
-
 class AtlaskitSelect extends React.Component<Field> {
-  constructor(props: Field) {
-    super(props);
-    const { registerField, onFieldChange, ...fieldDef } = props;
-    if (registerField) {
-      registerField(fieldDef);
-    } else {
-      console.warn(
-        'Could not register field because registerField function was missing',
-        fieldDef
-      );
-    }
-  }
   render() {
     const {
       disabled,
@@ -43,7 +16,6 @@ class AtlaskitSelect extends React.Component<Field> {
       required,
       value,
       label,
-      description,
       onFieldChange
     } = this.props;
     let defaultSelected;
@@ -79,34 +51,28 @@ class AtlaskitSelect extends React.Component<Field> {
     });
 
     return (
-      <Layout id={id}>
-        <SingleSelect
-          key={id}
-          name={name}
-          label={label}
-          defaultSelected={defaultSelected}
-          placeholder={placeholder}
-          disabled={disabled}
-          required={required}
-          isInvalid={!isValid}
-          value={stringValue}
-          items={items}
-          onSelected={evt => {
-            onFieldChange(id, evt.item.value);
-          }}
-        />
-        {description && (
-          <Tooltip content={description} position="right">
-            <InfoIcon primaryColor="#6554C0" />
-          </Tooltip>
-        )}
-      </Layout>
+      <SingleSelect
+        key={id}
+        name={name}
+        label={label}
+        defaultSelected={defaultSelected}
+        placeholder={placeholder}
+        disabled={disabled}
+        required={required}
+        isInvalid={!isValid}
+        value={stringValue}
+        items={items}
+        onSelected={evt => {
+          onFieldChange(id, evt.item.value);
+        }}
+      />
     );
   }
 }
 
 export default (props: FieldDef) => (
-  <FormContext.Consumer>
-    {form => <AtlaskitSelect {...form} {...props} />}
-  </FormContext.Consumer>
+  <FieldWrapper {...props}>
+    {/* $FlowFixMe */}
+    <AtlaskitSelect />
+  </FieldWrapper>
 );
